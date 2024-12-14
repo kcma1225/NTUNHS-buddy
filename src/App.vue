57ae087246a5b935b8360991822e630049d64f49
kcma1225 <157051689+kcma1225@users.noreply.github.com>
2024-12-15 01:21:06 +0800

    fen/fix login,news popup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app" class="flex flex-col min-h-screen">
+    <!-- Header -->
     <Header @open-login="showLoginPopup" @open-news="showNewsPopup" />
 
-    <!-- Main Content Frame -->
+    <!-- Main Frame -->
     <main class="flex-grow pt-16 bg-gray-100">
       <div class="container mx-auto p-6 bg-white shadow-lg rounded-lg">
+        <!-- 動態內容區域 -->
         <component :is="currentComponent" />
       </div>
     </main>
@@ -19,10 +21,15 @@
 </template>
 
 <script>
+/* View */
 import Header from "./components/header/Header.vue";
 import Footer from "./components/footer/footer.vue";
+
+/* Popup Frame */
 import LoginPopup from "./components/auth/LoginPopup.vue";
 import NewsPopup from "./components/header/NewsPopup.vue";
+
+/* Main Frame*/
 import ExampleComponent from "./components/ExampleComponent.vue";
 
 export default {
@@ -31,14 +38,38 @@ export default {
     Footer,
     LoginPopup,
     NewsPopup,
-    ExampleComponent,
+    ExampleComponent, // 預設載入一個範例組件
   },
   data() {
     return {
       isLoginVisible: false,
       isNewsVisible: false,
-      currentComponent: "ExampleComponent",
+      currentComponent: "ExampleComponent", // 預設組件
     };
+  },
+  methods: {
+    showLoginPopup() {
+      this.isLoginVisible = true;
+    },
+    hideLoginPopup() {
+      this.isLoginVisible = false;
+    },
+    showNewsPopup() {
+      this.isNewsVisible = true;
+    },
+    hideNewsPopup() {
+      this.isNewsVisible = false;
+    },
+    loadComponent(componentName) {
+      this.currentComponent = componentName; // 切換不同組件
+    },
   },
 };
 </script>
+
+<style>
+/* 固定 Header 高度，避免被遮擋 */
+.pt-16 {
+  padding-top: 4rem; /* Header 高度，16 x 0.25rem = 4rem */
+}
+</style>
